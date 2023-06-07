2225b668901762ae6e768a4e0bdbc0d096f68067
FernandoAndradeSilva <fernando.andrade.inf@gmail.com>
2023-06-07 14:17:23 -0300

    Primeiro commit

---
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -0,0 +1,19 @@
+package com.example.algamoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+
+@EnableConfigurationProperties(value = AlgamoneyApiProperty.class)
+@SpringBootApplication
+public class AlgamoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -0,0 +1,48 @@
+package com.example.algamoney.api.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("algamoney")
+public class AlgamoneyApiProperty {
+	
+	private String origemPermitida = "http://localhost:8000";
+	
+	private final Seguranca seguranca = new Seguranca();	
+	
+	public void setOrigemPermitida(String origemPermitida) {
+		this.origemPermitida = origemPermitida;
+	}
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}	
+
+	public String getOrigemPermitida() {
+		return origemPermitida;
+	}
+	
+	
+	
+	public static class Seguranca {
+		
+		private boolean enableHttps;
+
+		public boolean isEnableHttps() {
+			return enableHttps;
+		}
+
+		public void setEnableHttps(boolean enableHttps) {
+			this.enableHttps = enableHttps;
+		}		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
